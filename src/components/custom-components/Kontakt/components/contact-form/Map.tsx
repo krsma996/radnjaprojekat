d99ae385f1de3342f023a0e6e4e32516f7c0c326
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2024-11-14 18:15:22 +0100

    responsive form map and contact

--- src/components/custom-components/Kontakt/components/contact-form/Map.tsx
+++ src/components/custom-components/Kontakt/components/contact-form/Map.tsx
@@ -1,12 +1,13 @@
 export const ContactMap: React.FC = () => (
   <div className="kontakt-col">
-    <div style={{ marginTop: "20px" }}>
-      <h3 className="kontakt-h3">Naša lokacija</h3>
+    <h3 className="kontakt-h3">Naša lokacija</h3>
+    <div className="kontakt-map-container">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2830.123456789!2d20.37035550468595!3d44.81633739457389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a709ef5b12345%3A0x9876543210abc!2sBezanijska%20Kosa%202b%2C%20Beograd%2C%20Srbija!5e0!3m2!1sen!2srs!4v1234567890123"
-        width="1100"
-        height="450"
+        width="100%"
+        height="100%"
         loading="lazy"
+        className="kontakt-map-iframe"
       />
     </div>
   </div>
